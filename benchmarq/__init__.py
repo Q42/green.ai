551aeb7a781287aab0451fa86d52e6cea355279a
Kalle Pronk <kalle.pronk@q42.nl>
2025-04-10 16:22:01 +0200

    add metrics and experiment imports to benchmarq init file

--- benchmarq/__init__.py
+++ benchmarq/__init__.py
@@ -1,2 +1,5 @@
+from .custom_metrics.conversation_coherence.conversation_coherence import ConversationCoherenceMetric
+from .experiment import Experiment
+
 __app_name__ = "benchmarq"
 __version__ = "0.1.0"
