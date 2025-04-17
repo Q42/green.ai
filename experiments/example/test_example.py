d30b2ffbfd0bccd94f217afe53085144787d5011
Kalle Pronk <kalle.pronk@q42.nl>
2025-04-18 00:38:52 +0200

    refactor example to work with benchmark.py

--- experiments/example/test_example.py
+++ experiments/example/test_example.py
@@ -1,30 +1,21 @@
-import uuid
-
 import pytest
-from deepeval.dataset import Golden
-from deepeval.test_case import LLMTestCase
-from benchmarq.experiment import Experiment
+
+from benchmarq.benchmark import *
 
 
 @pytest.fixture
 def evaluate_test_case(async_client, model_config):
     """Create the evaluation function for the experiment."""
 
-    async def _evaluate(data: Golden) -> LLMTestCase:
+    async def _evaluate(row) -> str:
         output = await async_client.chat.completions.create(
             model=model_config["model"],
             messages=[
-                {"role": "system", "content": f"{data.context}"},
-                {"role": "user", "content": f"{data.input}"},
+                {"role": "system", "content": f"{row['context']}"},
+                {"role": "user", "content": f"{row['input']}"},
             ],
         )
-        output = output.choices[0].message.content
-        return LLMTestCase(
-            input=data.input,
-            expected_output=data.expected_output,
-            actual_output=output,
-            context=data.context,
-            retrieval_context=data.retrieval_context)
+        return output.choices[0].message.content
 
     return _evaluate
 
@@ -32,22 +23,12 @@ def evaluate_test_case(async_client, model_config):
 @pytest.mark.asyncio
 @pytest.mark.experiment
 async def test_example(evaluate_test_case, debug_mode, settings, metadata):
+    config = settings["beatles"]
 
-    metadata.update({"hello": "world"})
-    """Test energy consumption with different input sizes."""
-    experiment = Experiment(
-        id=uuid.uuid4().hex,
-        name="example",
-        dataset_name="beatles",
-        description="An example test to show how benchmarks are written",
-        subquestion_id="example",
-        settings=settings,
-        c_func=evaluate_test_case,
-        skip_metrics=True,
-        debug_mode=debug_mode,
-        metadata=metadata,
-    )
+    dataset = get_dataset(config)
 
-    result = await experiment.run()
-    assert result is not None
-    assert result.consumption_results is not None
+    _, consumption = await run(dataset, evaluate_test_case)
+
+    export_results("example", metadata, config, consumption)
+
+    assert consumption is not None
